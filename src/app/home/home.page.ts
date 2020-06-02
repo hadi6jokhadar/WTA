e20b984d8178f11706d6c0aa8dd508b9bd02429c
hadi6jokhadar <hadi6jokhadar@hotmail.com>
2020-06-03 00:18:58 +0300

    Convert Web to App Ionic

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,43 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import {
+  InAppBrowserOptions,
+  InAppBrowser,
+} from "@ionic-native/in-app-browser/ngx";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
-
-  constructor() {}
-
+  website = "https://www.google.com/";
+  options: InAppBrowserOptions = {
+    location: "no",
+    hidden: "no",
+    zoom: "no",
+    hideurlbar: "yes",
+    toolbar: "no",
+  };
+  private browser;
+  constructor(private _inAppBrowser: InAppBrowser) {
+    this.start();
+  }
+  private start() {
+    this.browser = this._inAppBrowser.create(
+      this.website,
+      "_blank",
+      this.options
+    );
+    if (this.browser.on("loadstart")) {
+      this.browser.on("loadstart").subscribe((event) => {
+        // console.log(event.url);
+        // alert(event.url);
+      });
+    }
+    if (this.browser.on("exit")) {
+      this.browser.on("exit").subscribe((event) => {
+        navigator["app"].exitApp();
+      });
+    }
+  }
 }
